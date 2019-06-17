da343ed05f199a47eb26e3bf33e270a4e3b7373e
fukakachi <51438365+fukakachi@users.noreply.github.com>
2019-06-17 19:53:15 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-{\rtf1}
+//クリックしたときの動き
+$('').click(function(){
+  $('').show();
+});
+//ポインタがのった時の動き
+$('').hover(
+  $('').show();,
+  $('').hide();
+);
+//画像のスライド
+$('').ready(function(){
+  $('').bxSlider({
+    auto: true,
+    pause: 5000,
+  });
+});
