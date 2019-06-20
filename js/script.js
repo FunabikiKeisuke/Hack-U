83f991af1e752417cfb7b971e5b5c2056346c756
fukakachi <51438365+fukakachi@users.noreply.github.com>
2019-06-20 20:22:10 +0900

    スライドショー

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-//クリックしたときの動き
-$('.1').click(function() {
-	$('#pop-content1').show();
-});
-$('.2').click(function() {
-	$('#pop-content2').show();
-});
 
-//画像のスライドショー
 $(function(){
-var page=0;
-var lastPage=parseInt($("#slide img").length-1);
 
+
+  //（１）ページの概念・初期ページを設定
+  var page=0;
+
+  //（２）イメージの数を最後のページ数として変数化
+  var lastPage =parseInt($("#slide img").length-1);
+
+  //（３）最初に全部のイメージを一旦非表示にします
+  $("#slide img").css("display","none");
+
+  //（４）初期ページを表示
+  $("#slide img").eq(page).css("display","block");
+
+  //（５）ページ切換用、自作関数作成
+  function changePage(){
+    $("#slide img").fadeOut(1000);
+    $("#slide img").eq(page).fadeIn(1000);
+  };
+
+  //（６）～秒間隔でイメージ切換の発火設定
+  var Timer;
+  function startTimer(){
+    Timer =setInterval(function(){
+      if(page === lastPage){
+        page = 0;
+        changePage();
+      }else{
+        page ++;
+        changePage();
+      };
+    },5000);
+  }
+  //（７）～秒間隔でイメージ切換の停止設定
+  function stopTimer(){
+    clearInterval(Timer);
+  }
+
+  //（８）タイマースタート
+  startTimer();
+
+  /*オプションを足す場合はここへ記載*/
 
 });
-
-// レビュー
-$('textarea').autoResize();
