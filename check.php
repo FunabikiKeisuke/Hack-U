1813fceb28cff2a04c88bb4bad02693924d543e3
FunabikiKeisuke <keisuke.funabiki.official@gmail.com>
2019-06-27 00:53:48 +0900

    signup.php完成

---
+++ check.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Signup</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+			integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+			integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/login.css">
+	</head>
+	<body>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="container">
+						<!-- form start -->
+						<form action="" method="post" class="form-container">
+							<legend>内容確認</legend>
+							<div class="form-group 6">
+								<label for="exampleInputName">氏名</label>
+							</div>
+							<div class="form-group 6">
+								<label for="exampleInputEmail">メールアドレス</label>
+							</div>
+							<div class="form-group 6">
+								<label for="exampleInputPassword">パスワード</label>
+								<p>【表示されません】</p>
+							</div>
+							<div class="button">
+								<a href="signup.php?action=rewrite">&laquo;&nbsp;書き直す</a><br>
+								<input type="submit" value="登録する" class="btn btn-default btn-block" >
+							</div>
+						<!-- form end -->
+					</div>
+				</div>
+			</div>
+		<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"crossorigin="anonymous"></script>
+	</body>
+</html>
