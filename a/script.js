580c039001285d8da57219531b289c460e4d77af
adrianrio <48291070+adrianrio@users.noreply.github.com>
2019-06-26 16:07:57 +0900

    写真を変えた

--- a/script.js
+++ a/script.js
@@ -1,45 +1,42 @@
-$(function(){
+$(function() {
+	//（１）ページの概念・初期ページを設定
+	var page = 0;
 
+	//（２）イメージの数を最後のページ数として変数化
+	var lastPage = parseInt($('#slide img').length - 1);
 
-  //（１）ページの概念・初期ページを設定
-  var page=0;
+	//（３）最初に全部のイメージを一旦非表示にします
+	$('#slide img').css('display', 'none');
 
-  //（２）イメージの数を最後のページ数として変数化
-  var lastPage =parseInt($("#slide img").length-1);
+	//（４）初期ページを表示
+	$('#slide img').eq(page).css('display', 'block');
 
-  //（３）最初に全部のイメージを一旦非表示にします
-  $("#slide img").css("display","none");
+	//（５）ページ切換用、自作関数作成
+	function changePage() {
+		$('#slide img').fadeOut(1000);
+		$('#slide img').eq(page).fadeIn(1000);
+	}
 
-  //（４）初期ページを表示
-  $("#slide img").eq(page).css("display","block");
+	//（６）～秒間隔でイメージ切換の発火設定
+	var Timer;
+	function startTimer() {
+		Timer = setInterval(function() {
+			if (page === lastPage) {
+				page = 0;
+				changePage();
+			} else {
+				page++;
+				changePage();
+			}
+		}, 1000);
+	}
+	//（７）～秒間隔でイメージ切換の停止設定
+	function stopTimer() {
+		clearInterval(Timer);
+	}
 
-  //（５）ページ切換用、自作関数作成
-  function changePage(){
-    $("#slide img").fadeOut(1000);
-    $("#slide img").eq(page).fadeIn(1000);
-  };
+	//（８）タイマースタート
+	startTimer();
 
-  //（６）～秒間隔でイメージ切換の発火設定
-  var Timer;
-  function startTimer(){
-    Timer =setInterval(function(){
-      if(page === lastPage){
-        page = 0;
-        changePage();
-      }else{
-        page ++;
-        changePage();
-      };
-    },1000);
-  }
-  //（７）～秒間隔でイメージ切換の停止設定
-  function stopTimer(){
-    clearInterval(Timer);
-  }
-
-  //（８）タイマースタート
-  startTimer();
-
-  /*オプションを足す場合はここへ記載*/
-
+	/*オプションを足す場合はここへ記載*/
 });
